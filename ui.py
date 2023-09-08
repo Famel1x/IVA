c9da827b61a4ae26ea43cc1a722dce88b230827f
HirikaYato <hirikayato@mail.ru>
2023-09-08 23:52:38 +0300

    App UI

---
+++ ui.py
@@ -0,0 +1,140 @@
+from voice import Voice
+import flet as ft
+import os
+
+voice = Voice()
+
+def getSettings(type: str):
+    if not os.path.isfile("ui_config.ini"):
+            with open("ui_config.ini", "w") as file:
+                file.write("Theme = dark\n")
+            print("Was created ui_config.ini")
+    
+    with open("ui_config.ini", 'r') as file:
+        theme = file.readline()[8:-1].lower()
+
+        if type == "theme": return theme
+
+def main(page: ft.Page):
+    page.title = "Voice assistant"
+    page.window_width = 420
+    page.window_height = 500
+    page.theme_mode = getSettings("theme")
+
+    def speech():
+        voice.textToSpeech("Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!Привет мир!")
+
+    def pick_files_result(e: ft.FilePickerResultEvent):
+        try:
+            selected_files.value = (
+            "\n".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
+            )
+            selected_files.update()
+        except:
+            pass
+
+    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
+    selected_files = ft.Text(size = 20)
+
+    page.overlay.append(pick_files_dialog)
+
+    tab_assistant = ft.Container(
+        ft.Stack([
+            ft.Column(
+                [
+                ft.Container(
+                    ft.Stack([
+                        ft.Column([
+                            ft.IconButton(icon = ft.icons.KEYBOARD_VOICE, icon_size = 30, bgcolor = ft.colors.BLUE,
+                            on_click = lambda x: speech()),
+                        ],
+                        top = 200,
+                        left = 160)
+                    ]),
+                    border_radius = 40,
+                    padding = 5,
+                    width = 390,
+                    height = 380,
+                    bgcolor = ft.colors.BLACK26)
+                ],
+                top = 15)
+        ]), 
+        bgcolor = ft.colors.TRANSPARENT)
+    
+    tab_upload = ft.Container(
+        ft.Stack([
+            ft.Column(
+                [
+                ft.Container(
+                    ft.Stack([
+                        ft.Row([
+                            ft.OutlinedButton(text="Выбрать файлы",
+                                on_click = lambda x: pick_files_dialog.pick_files(allow_multiple=True)),
+                            
+                            ft.IconButton(icon = ft.icons.FILE_UPLOAD, )
+                        ],
+                        top = 15,
+                        left = 100),
+
+                        ft.Column([
+                            selected_files],
+                        top = 60,
+                        left = 20)
+                    ]),
+                    border_radius = 40,
+                    padding = 5,
+                    width = 390,
+                    height = 380,
+                    bgcolor = ft.colors.BLACK26)
+                ],
+                top = 15)
+        ]), 
+        bgcolor = ft.colors.TRANSPARENT)
+
+    tab_settings = ft.Container(
+        ft.Stack([
+            ft.Column(
+                [
+                ft.Container(
+                    ft.Stack([
+                        ft.Column([
+                            ft.Switch(label = "Темная тема", value = True if getSettings("theme") == "dark" else False)
+                        ],
+                        top = 20,
+                        left = 20)
+                    ]),
+                    border_radius = 40,
+                    padding = 5,
+                    width = 390,
+                    height = 380,
+                    bgcolor = ft.colors.BLACK26)
+                ],
+                top = 15)
+        ]), 
+        bgcolor = ft.colors.TRANSPARENT)
+
+    tabs = ft.Tabs(
+            selected_index = 0,
+            animation_duration = 300,
+            tabs=[
+                ft.Tab(
+                    text = "Асистент",
+                    icon = ft.icons.KEYBOARD_VOICE,
+                    content = tab_assistant),
+                ft.Tab(
+                    text = "Загрузить",
+                    icon = ft.icons.UPLOAD_FILE_OUTLINED,
+                    content = tab_upload,
+                ),
+                ft.Tab(
+                    text = "Настройки",
+                    icon = ft.icons.SETTINGS,
+                    content = tab_settings),
+            ],
+            expand=1,
+        )
+
+    page.add(tabs)
+    page.update()
+
+ft.app(target=main)
---
+++ voice.py
@@ -0,0 +1,62 @@
+import openai
+import os
+import torch
+import keyboard
+import speech_recognition # для работы требуется установить pyaudio
+from playsound import playsound #pip install playsound==1.2.2
+from IPython.display import Audio
+from googletrans import Translator # pip install googletrans==3.1.0a0
+
+class Voice:
+    def __init__(self):
+        if not os.path.isdir("TTS results"):
+            os.mkdir("TTS results")
+            print("Was created folder TTS results")
+
+        for file in os.listdir("./TTS results/"):
+            if file.endswith(".wav"):
+                os.remove(f"TTS results/{file}")
+
+    answer_number = 0
+    resultSTT = ""
+    resultGPT = ""
+
+    recognizer = speech_recognition.Recognizer()
+    translator = Translator(service_urls=['translate.googleapis.com'])
+
+    model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
+                                        model='silero_tts',
+                                        language= 'ru',
+                                        speaker= 'v3_1_ru')
+    model.to(torch.device('cpu'))
+
+    def textToSpeech(self, text):
+        print("Ответ на фразу: " + text)
+
+        audio = self.model.apply_tts(text=text,
+                            speaker= 'xenia',
+                            sample_rate= 48000,
+                            put_accent= True,
+                            put_yo= True)
+
+        with open(f'TTS results/voiceAnswer{self.answer_number}.wav', 'wb') as file:
+            file.write(Audio(audio, rate= 48000).data)
+
+        playsound(f'TTS results/voiceAnswer{self.answer_number}.wav')
+        self.answer_number += 1
+
+    def speechToText(self, deviceIndex: int = 1):
+        while True:
+                print("button is pressed")
+                try:
+                    with speech_recognition.Microphone(device_index = deviceIndex) as sourse:
+                        audio = self.recognizer.listen(sourse)
+                except:
+                    print("Ошибка инициализации микрофона")
+                try:
+                    self.resultSTT = self.recognizer.recognize_google(audio, language = "ru-RU").lower()
+                except:
+                    print("Ошибка распознования аудио")
+                
+                print("Распознаная фраза: " + self.resultSTT)
+                return self.resultSTT
