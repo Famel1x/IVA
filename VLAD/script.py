e7e2464df625f372bb0f4ce37b7dc63e7f56758e
HirikaYato <hirikayato@mail.ru>
2023-09-09 14:35:16 +0300

    123

--- VLAD/script.py
+++ VLAD/script.py
@@ -4,7 +4,7 @@ wb = openpyxl.load_workbook('VLAD/20.xlsx')
 
 sheet = wb.get_sheet_by_name('Лист1')
 
-with open("VLAD/20.txt", "w", encoding = "UTF-8") as file:
+with open("VLAD/20.yml", "w", encoding = "UTF-8") as file:
     i = 1
 
     while sheet[f'B{i}'].value != "end":
@@ -18,23 +18,23 @@ with open("VLAD/20.txt", "w", encoding = "UTF-8") as file:
                 if sheet[f"C{i + 1}"].value == None:
                     file.write(f"--{sheet[f'B{i}'].value}" + " " + f"{sheet[f'C{i}'].value}\n")
 
-                    for j in range(i, k - 1):
-                        file.write(f"-{sheet[f'D{i}'].value}" + "\n")
+                    for j in range(i, k):
+                        file.write(f" -{sheet[f'D{j}'].value}" + "\n")
                         
                 else:
                     file.write(f"--{sheet[f'B{i}'].value}")
 
-                    for j in range(i, k - 1):
+                    for j in range(i, k):
                         if sheet[f'C{i}'].value != None:  
-                            file.write(f"-{sheet[f'C{i}'].value}" + "\n")
+                            file.write(f" -{sheet[f'C{j}'].value}" + "\n")
 
-                    for j in range(i, k - 1):
-                        file.write(f"-{sheet[f'D{i}'].value}" + "\n")
+                    for j in range(i, k):
+                        file.write(f" -{sheet[f'D{j}'].value}" + "\n")
 
             else:
                 file.write(f"--{sheet[f'B{i}'].value}" + " " + f"{sheet[f'C{i}'].value}\n")
-                file.write(f"-{sheet[f'D{i}'].value}" + "\n")
+                file.write(f" -{sheet[f'D{i}'].value}" + "\n")
 
-        i = i + 1
-        print(i)
-        if i == 900: exit(0)
+        print(f"{i} {k}")
+        i = k
+        if i == 3000: exit(0)
