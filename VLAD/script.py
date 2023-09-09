8c3baf0ca15d05c463ba23e28ff91773020d3a03
HirikaYato <hirikayato@mail.ru>
2023-09-09 16:28:34 +0300

    123

--- VLAD/script.py
+++ VLAD/script.py
@@ -22,38 +22,38 @@ def parser(file):
                 if i != k - 1:
                     if sheet[f"C{i + 1}"].value == None: 
                             
-                        string = f"- - {sheet[f'B{i}'].value}".replace(".", "") + " " + f"{sheet[f'C{i}'].value}".replace(".", "")
+                        string = f"{sheet[f'B{i}'].value}".replace(".", "").strip() + " " + f"{sheet[f'C{i}'].value}".replace(".", "").strip()
 
-                        file.write(string + "?\n")
+                        file.write("- - " + string + "?\n")
 
                         for j in range(i, k):
-                            string = f"  - {sheet[f'D{j}'].value}".replace(".", "")
+                            string = f"{sheet[f'D{j}'].value}".replace(".", "").strip()
 
-                            file.write(string + ".\n")
+                            file.write("  - " + string + ".\n")
                             
                     else:
-                        string = f"- - {sheet[f'B{i}'].value}".replace(".", "")
+                        string = f"{sheet[f'B{i}'].value}".replace(".", "").strip()
 
-                        file.write(string + "?\n")
+                        file.write("- - " + string + "?\n")
 
                         for j in range(i, k):
                             if sheet[f'C{i}'].value != None:  
                                 
-                                string = f"  - {sheet[f'C{j}'].value}".replace(".", "")
-                                file.write(string + ".\n")
+                                string = f"  - {sheet[f'C{j}'].value}".replace(".", "").strip()
+                                file.write("  - " + string + ".\n")
 
                         for j in range(i, k):
-                            string = f"  - {sheet[f'D{j}'].value}".replace(".", "")
-                            file.write(string + ".\n")
+                            string = f"{sheet[f'D{j}'].value}".replace(".", "").strip()
+                            file.write("  - " + string + ".\n")
 
                 else:
-                    string = f"- - {sheet[f'B{i}'].value}".replace(".", "") + " " + f"{sheet[f'C{i}'].value}".replace(".", "")
+                    string = f"{sheet[f'B{i}'].value}".replace(".", "").strip() + " " + f"{sheet[f'C{i}'].value}".replace(".", "").strip()
 
-                    file.write(string + "?\n")
+                    file.write("- - " + string + "?\n")
                     
-                    string = f"  - {sheet[f'D{i}'].value}".replace(".", "")
+                    string = f"{sheet[f'D{i}'].value}".replace(".", "").strip()
 
-                    file.write(string + ".\n")
+                    file.write("  - " + string + ".\n")
 
             print(f"{i} {k}")
             i = k
