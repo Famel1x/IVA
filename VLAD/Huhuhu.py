f345b10d4fe5dfcb0b187abccd900912ffbb0e78
Qbi8 <ivan-nn@bk.ru>
2023-09-10 04:05:44 +0300

    ljll

--- VLAD/Huhuhu.py
+++ VLAD/Huhuhu.py
@@ -33,8 +33,8 @@ for i in range(0, len(input_str)):
                 istr2=""
                 for s in range(start, end):
                     istr2=istr2+input_str[s]
-                input_str=input_str[:start] + " " + convert_numbers_to_words(istr2) + " " + input_str[end:]
-                print(input_str)
+input_str=input_str[:start] + " " + convert_numbers_to_words(istr2) + " " + input_str[end:]
+print(input_str)
        
        
 
